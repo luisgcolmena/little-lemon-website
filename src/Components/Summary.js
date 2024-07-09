fe7fe01b598d249f36f78eb35e50c5043e03924d
luisgcolmena <luisgcolmena@gmail.com>
2024-07-09 17:56:15 -0400

    Agregando media queries

--- src/Components/Summary.js
+++ src/Components/Summary.js
@@ -3,27 +3,29 @@ import '../Styles/Summary.css'
 function Summary() {
   return (
     <section className='summary-section'>
-      <h1>Rservation Summary</h1>
-      <div className='summary-container'>
-        <div className='summary-item'>
-          <p>Date:</p>
-          <p>25/07/2024</p>
-        </div>
-        <div className='summary-item'>
-          <p>Table:</p>
-          <p>Outside / 13:00</p>
-        </div>
-        <div className='summary-item'>
-          <p>N° Diners:</p>
-          <p>5</p>
-        </div>
-        <div className='summary-item'>
-          <p>Notes:</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-        <div className='summary-item'>
-          <p>Occasion:</p>
-          <p>Birthday</p>
+      <div className='summary-section-container'>
+        <h1>Reservation Summary</h1>
+        <div className='summary-container'>
+          <div className='summary-item'>
+            <p>Date:</p>
+            <p>25/07/2024</p>
+          </div>
+          <div className='summary-item'>
+            <p>Table:</p>
+            <p>Outside / 13:00</p>
+          </div>
+          <div className='summary-item'>
+            <p>N° Diners:</p>
+            <p>5</p>
+          </div>
+          <div className='summary-item'>
+            <p>Notes:</p>
+            <p className='summary-notes'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+          <div className='summary-item'>
+            <p>Occasion:</p>
+            <p>Birthday</p>
+          </div>
         </div>
       </div>
     </section>
