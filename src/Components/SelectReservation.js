f59340a720c7b84eb59c73d1f139a9e1c5e4eee1
Luis Gerardo <luisgcolmena@gmail.com>
2024-07-09 21:32:10 -0400

    Agregando estilos responsive

--- src/Components/SelectReservation.js
+++ src/Components/SelectReservation.js
@@ -5,8 +5,12 @@ function SelectReservation () {
     <section className='selection-section'>
       <h1>Select Date </h1>
       <button>Date: 25/07/2024</button>
-      <h1>Select Time</h1>
+      <h1>Select Time and Table</h1>
+      <p>Select your preferred table area: cozy indoor seating or delightful outdoor dining in the open air.</p>
       <button>Time: 13:00</button>
+      <p>Select the number of diners:</p>
+      <button>N° Diners</button>
+      <button>Occasion</button>
     </section>
   )
 }
