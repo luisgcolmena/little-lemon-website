65b820863e2e96897047b08e5e01f81290db2d61
luisgcolmena <luisgcolmena@gmail.com>
2024-08-22 16:47:03 -0400

    Agregando estilos del Booking Form

--- src/Components/BookingPage.js
+++ src/Components/BookingPage.js
@@ -3,34 +3,42 @@ import '../Styles/BookingPage.css'
 function BookingPage () {
   return (
     <form className='booking-form'>
-      <label htmlFor='date-input'>Date</label>
-      <input type='date' id='date-input'/>
+      <div className='date-input'>
+        <label htmlFor='date-input'><h2>Select Date</h2></label>
+        <input type='date' id='date-input'/>
+      </div>
 
-      <label htmlFor='time'>Time</label>
-      <select id='time'>
-        <option>12:00</option>
-        <option>13:00</option>
-        <option>14:00</option>
-        <option>18:00</option>
-        <option>19:00</option>
-        <option>20:00</option>
-      </select>
+      <div className='time-input'>
+        <label htmlFor='time'><h2>Time</h2></label>
+        <select id='time'>
+          <option>12:00</option>
+          <option>13:00</option>
+          <option>14:00</option>
+          <option>18:00</option>
+          <option>19:00</option>
+          <option>20:00</option>
+        </select>
+      </div>
 
-      <label htmlFor='guests'>Guests</label>
-      <input
-        type='number'
-        placeholder='1'
-        min={1}
-        max={10}
-        id='guests'/>
+      <div className='guests-input'>
+        <label htmlFor='guests'><h3>Guests</h3></label>
+        <input
+            type='number'
+            placeholder='1'
+            min={1}
+            max={10}
+            id='guests'/>
+      </div>
 
-        <label>Occasion</label>
-        <select placeholder='Select Occasion'>
+      <div className='ocassion-input'>
+        <label htmlFor='ocassion'><h3>Occasion</h3></label>
+        <select id='ocassion' placeholder='Select Occasion'>
           <option>Birthday</option>
           <option>Anniversary</option>
           <option>Engagement</option>
           <option selected>Simple meal</option>
         </select>
+      </div>
     </form>
   )
 }
