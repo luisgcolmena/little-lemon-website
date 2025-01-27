58a3d1766990f34766af19d73c21c60a5c81bb36
luisgcolmena <luisgcolmena@gmail.com>
2025-01-27 16:53:02 -0300

    Test: agregando pruebas para todos los inputs

--- src/Components/BookingPage/BookingPage.test.js
+++ src/Components/BookingPage/BookingPage.test.js
@@ -1,5 +1,5 @@
 import BookingPage from "./BookingPage"
-import {render, fireEvent, screen, waitFor } from '@testing-library/react'
+import {render, screen } from '@testing-library/react'
 import { MemoryRouter } from "react-router-dom"
 import { userEvent } from '@testing-library/user-event'
 
@@ -12,7 +12,7 @@ const getInputByLabel = (inputLabel) => {
 
 //does not call onSubmit if the form is incomplete
 
-describe('renders form with initial values', () => {
+/* describe('renders form with initial values', () => {
 
   function MemoBookingPage () {
     return (
@@ -22,23 +22,48 @@ describe('renders form with initial values', () => {
     )
   }
 
-
   test('date input', () => {
-
     render(<MemoBookingPage />)
-
     const dateInput = getInputByLabel('Date')
-    expect(dateInput).toHaveValue('')
+    expect(dateInput.value).toBe('')
+  })
+
+  test('time input', () => {
+    render(<MemoBookingPage />)
+    const timeInput = getInputByLabel('Time')
+    expect(timeInput.value).toBe('17:00')
+  })
+
+  test('guests input', () => {
+    render(<MemoBookingPage />)
+    const guestsInput = getInputByLabel('Guests')
+    expect(guestsInput.value).toBe('1')
+  })
+
+  test('ocassion input', () => {
+    render(<MemoBookingPage />)
+    const ocassionInput = getInputByLabel('Ocassion')
+    expect(ocassionInput.value).toBe('Birthday')
   })
 
   test('name input', () => {
-
     render(<MemoBookingPage />)
-
     const nameInput = getInputByLabel('Full name')
     expect(nameInput).toHaveValue('Luis G')
   })
-})
+
+  test('email input', () => {
+    render(<MemoBookingPage />)
+    const emailInput = getInputByLabel('Email')
+    expect(emailInput.value).toBe('')
+  })
+
+  test('phone number input', () => {
+    render(<MemoBookingPage />)
+    const phoneNumberInput = getInputByLabel('Phone number')
+    expect(phoneNumberInput.value).toBe("")
+  })
+}) */
 
 describe ('update states when typing inputs', () => {
 
@@ -50,11 +75,10 @@ describe ('update states when typing inputs', () => {
     )
   }
 
+  //Date input
   test('date input', async () => {
 
     render(<MemoBookingPage />)
-
-    //Date input
     const dateInput = getInputByLabel('Date')
     await userEvent.clear(dateInput)
     await userEvent.type(dateInput, '2025-07-25')
@@ -62,16 +86,46 @@ describe ('update states when typing inputs', () => {
 
   })
 
-  test('name input', async () => {
-
+  //Guests input
+  test('guests input', async () => {
     render(<MemoBookingPage />)
+    const guestsInput = getInputByLabel('Guests')
+    await userEvent.clear(guestsInput)
+    await userEvent.type(guestsInput, '7')
+    expect(guestsInput.value).toBe('7')
+  })
 
-    //Full name input
+  //Ocassion input
+  test('ocassion input', async () => {
+    render(<MemoBookingPage />)
+    const ocassionInput = getInputByLabel('Ocassion')
+    await userEvent.selectOptions(ocassionInput, 'Simple meal')
+    expect(ocassionInput.value).toBe('Simple meal')
+  })
+
+  //Full name input
+  test('name input', async () => {
+    render(<MemoBookingPage />)
     const nameInput = getInputByLabel('Full name')
     await userEvent.clear(nameInput)
     await userEvent.type(nameInput, 'Luis Gerardo')
     expect(nameInput.value).toBe('Luis Gerardo')
     })
+
+  //Email input
+  test('email input', async () => {
+    render(<MemoBookingPage />)
+    const emailInput = getInputByLabel('Email')
+    await userEvent.type(emailInput, 'luiscol620@gmail.com')
+    expect(emailInput.value).toBe('luiscol620@gmail.com')
+  })
+
+  test('phone number input', async () => {
+    render(<MemoBookingPage />)
+    const phoneInput = getInputByLabel('Phone number')
+    await userEvent.type(phoneInput, 'boop')
+    expect(phoneInput.value).toBe('9765')
+  })
 
 })
 
